6f8528282f5b0462eaa835a6ee8425db674a43b2
Archit0029 <architbishnoi177@gmail.com>
2025-06-11 18:47:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -97,6 +97,8 @@ def convert():
     except Exception as e:
         return f"Conversion error: {e}"
 
+# Bind to Render's dynamic port
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
     
